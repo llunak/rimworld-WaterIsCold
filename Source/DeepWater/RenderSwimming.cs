98ba46c245d3d5c7df0d7949b24e88e24b50cb25
Cozarkian <77899393+Cozarkian@users.noreply.github.com>
2021-10-24 07:42:01 -0700

    Add files via upload

--- Source/DeepWater/RenderSwimming.cs
+++ Source/DeepWater/RenderSwimming.cs
@@ -10,8 +10,8 @@ namespace WaterIsCold
     {
         public static void Prefix (PawnRenderer __instance, ref bool renderBody, bool portrait, Pawn ___pawn)
         {
-            Log.Message("rendering " + ___pawn.LabelShort);
-            if (portrait)
+            //Log.Message("rendering " + ___pawn.LabelShort);
+            if (portrait || !ModSettings_WaterIsCold.deepWater)
             {
                 return;
             }
@@ -19,30 +19,25 @@ namespace WaterIsCold
             {
                 return;
             }
-            if (___pawn.Dead || !___pawn.RaceProps.Humanlike || !ModSettings_WaterIsCold.deepWater)
+            if (___pawn.Dead || !___pawn.RaceProps.Humanlike)
             {
                 return;
             }
-            //try
-            //{
+            try
+            {
                 TerrainDef terrain = ___pawn.Position.GetTerrain(___pawn.Map);
                 bool deep = terrain != null && (terrain == TerrainDefOf.WaterMovingChestDeep || terrain == TerrainDefOf.WaterOceanDeep || terrain == TerrainDefOf.WaterDeep);
-                Log.Message("deep is " + deep + ", and renderBody is " + renderBody + " for " + ___pawn.LabelShort);
+                //Log.Message("deep is " + deep + ", and renderBody is " + renderBody + " for " + ___pawn.LabelShort);
                 if (renderBody && deep)
                 {
-                    Log.Message(___pawn.LabelShort + " should no longer have a body");
+                    //.Message(___pawn.LabelShort + " should no longer have a body");
                     renderBody = false;
                 }
-                else if (!renderBody && !deep)
-                {
-                    Log.Message("You thought this would never happen");
-                    renderBody = true;
-                }
-            //}
-            /*catch
+            }
+            catch
             {
                 //ignore
-            }*/
+            }
         }
     }
 }
--- Source/ModSettings_WaterIsCold.cs
+++ Source/ModSettings_WaterIsCold.cs
@@ -6,7 +6,7 @@ namespace WaterIsCold
     public class ModSettings_WaterIsCold : ModSettings
     {
         public static bool coldWater = true;
-        public static bool deepWater = true;
+        public static bool deepWater = false;
         public static bool funWater = true;
         public static bool disableWetAlways = false;
         public static bool disableWetWarm = false;
@@ -19,7 +19,7 @@ namespace WaterIsCold
         public override void ExposeData()
         {
             Scribe_Values.Look(ref coldWater, "coldWater", true);
-            Scribe_Values.Look(ref deepWater, "deepWater", true);
+            Scribe_Values.Look(ref deepWater, "deepWater", false);
             Scribe_Values.Look(ref funWater, "funWater", true);
             Scribe_Values.Look(ref disableWetAlways, "disableWetAlways", false);
             Scribe_Values.Look(ref disableWetWarm, "disableWetWarm", false);
