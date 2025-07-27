245923184113bcf68c02bd971d66744aff74e3b1
Luboš Luňák <l.lunak@centrum.cz>
2025-07-27 06:49:42 +0200

    Revert "remove deep water rendering"
    Apparently I do find this worth the effort.
    
    This reverts commit b4fac88583e8855404179605d16ebdfd41057552.

---
+++ Source/DeepWater/PawnRenderer.cs
@@ -0,0 +1,25 @@
+using System;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+
+namespace WaterIsCold
+{
+    [HarmonyPatch]
+    public class RenderSwimming
+    {
+        [HarmonyPatch(typeof(PawnRenderer), "RenderPawnInternal", new Type[] { typeof(Vector3), typeof(float), typeof(bool), typeof(RotDrawMode), typeof(bool), typeof(bool) })]
+        private static void Prefix(ref bool renderBody, Pawn ___pawn)
+        {
+            if (ModSettings_WaterIsCold.deepWater && renderBody && !___pawn.Swimming)
+            {
+                TerrainDef terrain = ___pawn.Position.GetTerrain(___pawn.Map);
+                if (terrain.IsWater && !Utility.IsShallowWater(terrain))
+                {
+                    renderBody = false;
+                }
+            }
+        }
+    }
+}
