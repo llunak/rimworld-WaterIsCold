ee22f9e9f72b9fafea933616150697fce349ee4f
Luboš Luňák <l.lunak@centrum.cz>
2025-07-27 23:02:13 +0200

    go back to using TryGainMemoryFast() for adding wet hediff
    The code in Pawn_MindState first calls CanGainGainThoughtNow(), then checks
    the roof and then calls TryGainMemoryFast(), so it is necessary to use
    the latter for adding the hediff, otherwise pawns get wet from rain even
    inside.

--- Source/ColdWater/AddWetHediff.cs
+++ Source/ColdWater/AddWetHediff.cs
@@ -3,58 +3,94 @@ using Verse;
 using Verse.AI;
 using UnityEngine;
 using HarmonyLib;
+using System;
 
 namespace WaterIsCold
 {
     [HarmonyPatch]
     public class AddWetHediff
     {
-        // Since this applies the hediff, this assumes that CanGainGainThoughtNow() is called
-        // immediately before gaining the thought, which is the case (at least as of 1.6).
+        private static bool lastCanGainThought = false;
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Pawn_MindState), nameof(CanGainGainThoughtNow))]
         public static bool CanGainGainThoughtNow(bool result, ThoughtDef thought, Pawn ___pawn)
         {
-            if (thought != ThoughtDefOf.SoakingWet)
-            {
-                // I assume this is some kind of a modded equivalent of SoakingWet.
-                if(ModLister.GetActiveModWithIdentifier("ReGrowth.BOTR.Core") == null)
-                    return result;
-                if(thought != DefDatabase<ThoughtDef>.GetNamedSilentFail("RG_Wet") && thought != DefDatabase<ThoughtDef>.GetNamedSilentFail("RG_ExtremelyWet"))
-                {
-                    return result;
-                }
-            }
-            if (ModSettings_WaterIsCold.coldWater)
-            {
-                AddHediff(___pawn);
-            }
+            if( !IsSoakingWetThought( thought ))
+                return result;
+            // Here we have to force the ability to gain the thought, since we handle getting wet during applying
+            // the thought (which first checks e.g. roof if the weather is causing the thought, and that check
+            // is not yet done here). Remember the real result for use when applying the thought (this function
+            // is always followed by applying the thought if it returns true for soaking wet).
+            lastCanGainThought = result;
+            return true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MemoryThoughtHandler), nameof(TryGainMemoryFast),
+            new Type[] { typeof( ThoughtDef ), typeof( Precept ) } )]
+        public static bool TryGainMemoryFast(ThoughtDef mem, Pawn ___pawn)
+        {
+            if( !IsSoakingWetThought( mem ))
+                return true; // Normal operation.
+            return HandleSoakingWetThought( ___pawn );
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MemoryThoughtHandler), nameof(TryGainMemoryFast),
+            new Type[] { typeof( ThoughtDef ), typeof( int ), typeof( Precept ) } )]
+        public static bool TryGainMemoryFast(ThoughtDef mem, int stage, Pawn ___pawn)
+        {
+            if( !IsSoakingWetThought( mem ))
+                return true; // Normal operation.
+            return HandleSoakingWetThought( ___pawn );
+        }
+
+        private static bool IsSoakingWetThought(ThoughtDef thought)
+        {
+            if (thought == ThoughtDefOf.SoakingWet)
+                return true;
+            // I assume this is some kind of a modded equivalent of SoakingWet.
+            if(ModLister.GetActiveModWithIdentifier("ReGrowth.BOTR.Core") == null)
+                return false;
+            return thought == DefDatabase<ThoughtDef>.GetNamedSilentFail("RG_Wet") || thought == DefDatabase<ThoughtDef>.GetNamedSilentFail("RG_ExtremelyWet");
+        }
+
+        // Apply the hediff on soaking wet thought (which is caused when the pawn is in water-related situation).
+        // Only after that check if the thought itself should be applied (which is independent, as the pawn
+        // may be in warm shallow water, which may be configured to not apply the thought, but still should cause the pawn to get somewhat wet).
+        private static bool HandleSoakingWetThought( Pawn pawn )
+        {
+            if( ModSettings_WaterIsCold.coldWater )
+                AddHediff(pawn);
 
             if( !ModSettings_WaterIsCold.soakingWetIfCold && !ModSettings_WaterIsCold.noSoakingWetIfShallowWarm )
-                return result;
-            float cellTemperature = GenTemperature.GetTemperatureForCell(___pawn.Position,___pawn.Map);
+                return lastCanGainThought; // We do not change whether the thought is gained.
+
+            float cellTemperature = GenTemperature.GetTemperatureForCell(pawn.Position,pawn.Map);
             if( ModSettings_WaterIsCold.noSoakingWetIfShallowWarm )
             {
-                TerrainDef terrain = ___pawn.Position.GetTerrain(___pawn.Map);
+                TerrainDef terrain = pawn.Position.GetTerrain(pawn.Map);
                 // Since the hediff raises the max comfy temperature, in this case use the original value (26C usually).
                 // Walking in shallow water when warmer than this is fine.
                 if( terrain.IsWater && Utility.IsShallowWater(terrain)
-                    && cellTemperature > ___pawn.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMax))
+                    && cellTemperature > pawn.def.GetStatValueAbstract(StatDefOf.ComfyTemperatureMax))
                 {
-                    return false;
+                    return false; // Skip thought gaining.
                 }
             }
             // Note that the comfortable temperature gets affected by the hediff, which is wanted (it'll quickly go from 16C to 21C
             // or worse). Make the pawn dislike the water if it's colder than that.
             if( ModSettings_WaterIsCold.soakingWetIfCold
-                && cellTemperature < ___pawn.GetStatValue(StatDefOf.ComfyTemperatureMin))
+                && cellTemperature < pawn.GetStatValue(StatDefOf.ComfyTemperatureMin))
             {
                 // Only if not fully vacuum resistant.
-                if( ___pawn.GetStatValue(StatDefOf.VacuumResistance, applyPostProcess: true, 5) < 1f )
-                    return true;
+                if( pawn.GetStatValue(StatDefOf.VacuumResistance, applyPostProcess: true, 5) < 1f )
+                    return true; // Gain the thought.
             }
-            return result;
+            return lastCanGainThought; // We do not change whether the thought is gained.
         }
+
 
         public static void AddHediff(Pawn pawn)
         {
