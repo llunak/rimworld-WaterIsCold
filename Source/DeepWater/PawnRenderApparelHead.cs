09bd2d54876e859f45d46f8ff03f72ce5ed89280
Luboš Luňák <l.lunak@centrum.cz>
2025-07-27 08:27:14 +0200

    implement hiding body when in deep water
    Does not work for e.g. animals, since they do not have a separate
    head, so this looks weird.

---
+++ Source/DeepWater/PawnRenderApparelHead.cs
@@ -0,0 +1,44 @@
+using System;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace WaterIsCold
+{
+    [HarmonyPatch(typeof(PawnRenderNodeWorker_Apparel_Head))]
+    public class PawnRenderNodeWorker_Apparel_Head_Patch
+    {
+        // Unfortunately this code disables rendering headger if PawnRenderFlags.Clothes is not set,
+        // even if PawnRenderFlags.Headgear itself is set. Fix that. Since there's presumably code
+        // that relies on this poor design of the flags being coupled, do this only for our use.
+        // Since PawnDrawParms is a struct (and thus gets passed by value), simply set the flag
+        // for it to nudge the code into allowing the headgear rendering.
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(CanDrawNow))]
+        static public void CanDrawNow(ref PawnDrawParms parms)
+        {
+            // Is it the problematic case?
+            if (!parms.flags.FlagSet(PawnRenderFlags.Clothes) || parms.flags.FlagSet(PawnRenderFlags.Headgear))
+            {
+                if( PawnRenderer_Patch.RenderAsInDeepWater( parms.pawn ))
+                    parms.flags |= PawnRenderFlags.Clothes;
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(HeadgearVisible))]
+        static public void HeadgearVisible(ref PawnDrawParms parms)
+        {
+            // Is it the problematic case?
+            if (!parms.flags.FlagSet(PawnRenderFlags.Clothes) || parms.flags.FlagSet(PawnRenderFlags.Headgear))
+            {
+                if( PawnRenderer_Patch.RenderAsInDeepWater( parms.pawn ))
+                    parms.flags |= PawnRenderFlags.Clothes;
+            }
+        }
+    }
+}
