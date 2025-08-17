c8ae791d34a37f3198f36ceda89ee3c7575d1f29
Luboš Luňák <l.lunak@centrum.cz>
2025-08-18 00:01:12 +0200

    disable the deep-water feature by default
    As said in its commit, it looks odd when pawns have their bodies
    hidden in deep water and animals do not. Odyssey introduced extra rendering
    for swimming animals, but it is not used for most of them, so if that
    gets completed, then it would make sense to enable this by default.

--- Source/ModSettings_WaterIsCold.cs
+++ Source/ModSettings_WaterIsCold.cs
@@ -6,7 +6,7 @@ namespace WaterIsCold
     public class ModSettings_WaterIsCold : ModSettings
     {
         public static bool coldWater = true;
-        public static bool deepWater = true;
+        public static bool deepWater = false;
         public static bool noHeadgearIfSwimming = true;
         public static bool soakingWetIfCold = true;
         public static bool noSoakingWetIfShallowWarm = true;
@@ -15,7 +15,7 @@ namespace WaterIsCold
         public override void ExposeData()
         {
             Scribe_Values.Look(ref coldWater, "coldWater", true);
-            Scribe_Values.Look(ref deepWater, "deepWater", true);
+            Scribe_Values.Look(ref deepWater, "deepWater", false);
             Scribe_Values.Look(ref noHeadgearIfSwimming, "noHeadgearIfSwimming", true);
             Scribe_Values.Look(ref soakingWetIfCold, "soakingWetIfCold", true);
             Scribe_Values.Look(ref noSoakingWetIfShallowWarm, "noSoakingWetIfShallowWarm", true);
